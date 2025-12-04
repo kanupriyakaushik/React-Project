1edf95a93fd469fa385cf1d4b5e77964b316102c
kanupriya kaushik <kaushikkanupriya81@gmail.com>
2025-12-04 13:27:06 +0530

    Src

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import Header from './components/Header/Header';
+import About from './components/About/About';
+import Projects from './components/Projects/Projects';
+import Skills from './components/Skills/Skills';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <Header />
+    <About />
+    <Projects />
+    <Skills />
+    </>
+
+  );
+}
+
